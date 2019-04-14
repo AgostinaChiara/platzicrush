9496a74b171b6763faefb411c84e5cb749590a0b
Agostina <agosschiara25@gmail.com>
2019-04-13 22:01:21 -0300

    Version 1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    menu,
+    inGame,
+    gameOver
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameState currentGameState = GameState.menu;
+
+    public static GameManager sharedInstance;
+
+    private PlayerController controller;
+
+    public int collectedObject = 0;
+
+    private void Awake()
+    {
+        if(sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Submit") && currentGameState != GameState.inGame)
+        {
+            StartGame();
+        } else if(Input.GetKeyDown(KeyCode.R))
+        {
+            GameOver();
+        } else if(Input.GetKeyDown(KeyCode.M))
+        {
+            BackToMenu();
+        }
+    }
+
+    public void StartGame()
+    {
+        SetGameState(GameState.inGame);
+        
+    }
+
+    public void GameOver()
+    {
+        SetGameState(GameState.gameOver);
+    }
+
+    public void BackToMenu()
+    {
+        SetGameState(GameState.menu);
+    }
+
+    void SetGameState(GameState newGameState)
+    {
+        if(newGameState == GameState.menu)
+        {
+            //TODO: colocar logica del menu
+            MenuManager.sharedInstance.ShowMainMenu();
+        } else if(newGameState == GameState.inGame)
+        {
+            //TODO: hay que preparar la escena para jugar
+            LevelManager.sharedInstance.RemoveAllLevelBlocks();
+            LevelManager.sharedInstance.GenerateInitialBlocks();
+            controller.StartGame();
+            MenuManager.sharedInstance.HideMainMenu();
+        } else if(newGameState == GameState.gameOver)
+        {
+            MenuManager.sharedInstance.ShowMainMenu();
+            //TODO: preparar el juego para el GameOver
+        }
+
+        this.currentGameState = newGameState;
+    }
+
+    public void CollectObject(Collectable collectable)
+    {
+        collectedObject += collectable.value;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 6f;
+    public float runningSpeed = 2f;
+    public float jumpRaycastDistance = 1.5f;
+
+    Rigidbody2D playerRb;
+    Animator animator;
+    Vector3 startPosition;
+
+    const string STATE_ALIVE = "isAlive";
+    const string STATE_ON_THE_GROUND = "isOnTheGround";
+    
+    [SerializeField]
+    private int healthPoints, manaPoints;
+
+    public const int INITIAL_HEALTH = 100, INITIAL_MANA = 15,
+        MAX_HEALTH = 200, MAX_MANA = 30,
+        MIN_HEALTH = 10, MIN_MANA = 0;
+
+    public const int SUPERJUMP_COST = 5;
+    public const float SUPERJUMP_FORCE = 1.5f;
+
+    public LayerMask groundMask;
+
+    void Awake()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = this.transform.position;
+    }
+
+    public void StartGame()
+    {
+        animator.SetBool(STATE_ALIVE, true);
+        animator.SetBool(STATE_ON_THE_GROUND, true);
+
+        healthPoints = INITIAL_HEALTH;
+        manaPoints = INITIAL_MANA;
+
+        Invoke("RestartPosition", 0.1f);
+    }
+
+    void RestartPosition()
+    {
+        this.transform.position = startPosition;
+        this.playerRb.velocity = Vector2.zero;
+        GameObject mainCamera = GameObject.Find("MainCamera");
+        mainCamera.GetComponent<CameraFollow>().ResetCameraPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump(false);
+        }
+        if (Input.GetButtonDown("SuperJump"))
+        {
+            Jump(true);
+        }
+
+        animator.SetBool(STATE_ON_THE_GROUND, IsGrounded());
+
+        Debug.DrawRay(this.transform.position, Vector2.down * jumpRaycastDistance, Color.red);
+
+    }
+
+    void FixedUpdate()
+    {
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+            if(playerRb.velocity.x < runningSpeed)
+            {
+                playerRb.velocity = new Vector2(runningSpeed, playerRb.velocity.y);
+            } else
+            {
+                playerRb.velocity = new Vector2(0, playerRb.velocity.y);
+            }
+        }
+    }
+
+    void Jump(bool superjump)
+    {
+        float jumpForceFactor = jumpForce;
+
+        if (superjump && manaPoints >= SUPERJUMP_COST)
+        {
+            manaPoints -= SUPERJUMP_COST;
+            jumpForceFactor *= SUPERJUMP_FORCE;
+        }
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+            if (IsGrounded())
+            {
+                GetComponent<AudioSource>().Play();
+                playerRb.AddForce(Vector2.up * jumpForceFactor, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    bool IsGrounded()
+    {
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, jumpRaycastDistance, groundMask))
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    public void Die()
+    {
+        float travelledDistance = GetTravelledDistance();
+        float previousMaxDistance = PlayerPrefs.GetFloat("maxscore", 0f);
+        if(travelledDistance > previousMaxDistance)
+        {
+            PlayerPrefs.SetFloat("maxscore", travelledDistance);
+        }
+
+        this.animator.SetBool(STATE_ALIVE, false);
+        GameManager.sharedInstance.GameOver();
+    }
+
+    public void CollectHealth(int points)
+    {
+        this.healthPoints += points;
+        if(this.healthPoints >= MAX_HEALTH)
+        {
+            this.healthPoints = MAX_HEALTH;
+        }
+
+        if(this.healthPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void CollectMana(int points)
+    {
+        this.manaPoints += points;
+        if (this.manaPoints >= MAX_MANA)
+        {
+            this.manaPoints = MAX_MANA;
+        }
+    }
+
+    public int GetHealth()
+    {
+        return healthPoints;
+    }
+
+    public int GetMana()
+    {
+        return manaPoints;
+    }
+
+    public float GetTravelledDistance()
+    {
+        return this.transform.position.x - startPosition.x;
+    }
+}
